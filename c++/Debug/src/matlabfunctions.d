ec0e93c2db70c64fcf913bb12406d2cc704932f8
Luca Cavazzana <lucacavazzana@users.noreply.github.com>
2011-01-18 15:54:08 +0000

    rimossi build file

--- c++/Debug/src/matlabfunctions.d
+++
@@ -1,9 +0,0 @@
-src/matlabfunctions.d src/matlabfunctions.o: ../src/matlabfunctions.cpp \
- ../src/matlabfunctions.h /usr/local/include/opencv/cv.h \
- /usr/local/include/opencv/highgui.h
-
-../src/matlabfunctions.h:
-
-/usr/local/include/opencv/cv.h:
-
-/usr/local/include/opencv/highgui.h:
