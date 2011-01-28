f1c7b4b1e07863417681ee94ea7e09b58867b53b
Luca Cavazzana <lucacavazzana@users.noreply.github.com>
2011-01-28 20:20:37 +0000

    added rmsContrast

---
+++ matlab/rmsContrast.m
@@ -0,0 +1,46 @@
+function [contr] = rmsContrast(feature, image)
+
+%[contr] = RMSCONTRAST(feature, image)
+% compute the root mean square contrast level around the feature in the
+% image
+%INPUT:
+%   'feature':  feature coords struct (x,y,z)
+%   'image':    image path or matrix
+%OUTPUT
+%   'contr':    rms contrast level
+
+
+n = 10; % feature window
+
+% Normalize
+if feature.z~=1
+    feature.x = feature.x/feature.z;
+    feature.y = feature.y/feature.z;
+end
+
+if size(image,1)==1 % if ==1 is a string...
+    image = imread(image);
+end
+if size(image,3)==3 % if ==3 RGB. FIXME: use the new function when isrgb will be replaced
+    image = rgb2gray(image);
+end
+
+% frame around the feature
+xi = max(round(feature.x - n),1);
+xf = min(round(feature.x + n),size(image,2));
+yi = max(round(feature.y - n),1);
+yf = min(round(feature.y + n),size(image,1));
+
+image = im2double(image(yi:yf,xi:xf));
+
+%--------------------------------------------------------------------------
+% Find root mean square contrast
+%--------------------------------------------------------------------------
+
+image = image(:); % make it vector
+m = mean(image);
+image = image - m;
+
+contr=norm(image)/sqrt(numel(image));
+
+end
