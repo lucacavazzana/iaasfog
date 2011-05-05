ba4013fea5549e86e11c64bfe9a963b0337351ab
Luca Cavazzana <lucacavazzana@users.noreply.github.com>
2011-05-05 10:32:59 +0000

    svn fix

--- matlab/estimateLamFit.m
+++ matlab/estimateLamFit.m
@@ -1,5 +1,6 @@
 function [] = estimateLamFit(feats, showPlot)
 
+
 if ~exist('showPlot','var')
     showPlot = 0;
 end
@@ -9,16 +10,14 @@ feats = normContrast(feats,'fitExp');
 
 if showPlot > 1
     for ff = feats
-        lamMinMax = minMaxContr(ff.contr, ff.tti);
+
         hold on; grid on;
         plot(ff.tti,ff.contr*ff.pars(1),'o');
         plot(ff.tti(1):.01:ff.tti(end), ff.pars(1)*exp(-(ff.tti(1):.01:ff.tti(end))/ff.pars(2)));
-        if lamMinMax ~= -1
-            plot(ff.tti(1):.01:ff.tti(end), ff.pars(1)*exp(-(ff.tti(1):.01:ff.tti(end))/lamMinMax),'g');
-        end
-        legend('data','fitted','minMax');
-        title(['fitted \lambda: ', num2str(ff.pars(2)), ', minMax \lambda: ', num2str(lamMinMax)]);
-        disp(['fitted lambda: ', num2str(ff.pars(2)), ', minMax lambda: ', num2str(lamMinMax)]);
+        
+        legend('data','fitted');
+        title(['fitted \lambda: ', num2str(ff.pars(2))]);
+        disp(['fitted lambda: ', num2str(ff.pars(2))]);
         pause;
         clf;
     end
