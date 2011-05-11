0fcf2215148beeaf76296ad70c311442da155ca6
LucaCavazzana <LucaCavazzana@df14c0a8-3095-3ac9-e2dc-079d0286a9dc>
2011-05-11 20:03:13 +0000

    doc update

--- trunk/matlab/test/printTests.m
+++ trunk/matlab/test/printTests.m
@@ -15,11 +15,8 @@ outFile = 'outFile.txt';
 % checks the image list
 imPaths = getPaths(imFolder, imName, imNum);
 
-alg = selectAlg({'inspect features'; ...
-    'plot contrasts'; ...
-    'estimate lambda by fitting'; ...
-    'normalize by fitted k and then ransac'; ...
-    'compare constrasts'});
+alg = selectAlg({'printFeats',...
+                'printFeats2'});
 
 if exist(outFile,'file')~=2
     
@@ -39,16 +36,10 @@ feats = parseFeatures(outFile, 1); % re-parsing features
 disp(['Found ', num2str(size(feats,2)), ' features over ', num2str(size(imPaths,1)), ' images']);
 
 switch alg
-    case 1, % visually check features
-        inspectFeatures(imPaths, feats);
-    case 2, % plots computed contrast
-        plotContrasts(feats);
-    case 3, % estimates lamdas by fitting on each single set
-        estimateLamFitTest(feats, 3);
-    case 4, % computes lambda normalizing by the fitted k and then applying ransac
-        fitNormRansacTest(feats, 3);
-    case 5, % compare different contrast formulas
-        compareContrasts(imPaths, feats, 0);
+    case 1,
+        printFeats(feats);
+    case 2,
+        normContrastTest(feats,'fitExp',3);
 end
 
 end
