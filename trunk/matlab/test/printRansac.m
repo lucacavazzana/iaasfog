a58bc5215da08ddccb14b6fc9f633bae4fc60b3f
LucaCavazzana <LucaCavazzana@df14c0a8-3095-3ac9-e2dc-079d0286a9dc>
2011-05-12 19:42:51 +0000

    doc update

--- trunk/matlab/test/printRansac.m
+++ trunk/matlab/test/printRansac.m
@@ -1,9 +1,9 @@
 function [] = printRansac(feats)
 
 feats = normContrast(feats, 'fitExp', 0);
-lam = [feats.pars]; lam = lam(2:2:end); % tmp var, not the output
-feats(lam > prctile(lam,80)) = [];
+% lam = [feats.pars]; lam = lam(2:2:end); % tmp var, not the output
+% feats(lam > prctile(lam,80)) = [];
 
-[par, mod, err] = myRansacTest(feats);
+myRansacTest(feats);
 
 end
