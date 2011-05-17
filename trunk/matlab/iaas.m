7bece1a3e98cff3104b409d38dcd2aea288e49ee
LucaCavazzana <LucaCavazzana@df14c0a8-3095-3ac9-e2dc-079d0286a9dc>
2011-05-17 19:26:22 +0000

    wannabe last update

--- trunk/matlab/iaas.m
+++ trunk/matlab/iaas.m
@@ -18,7 +18,11 @@ function [lam] = iaas(showPlot)
 % OPTIONS
 GUI = 0;    % if 0 use our default test values
 
-!rm outFile.txt
+try
+%     !rm outFile.txt
+catch e
+    
+end
 
 if ~exist('showPlot','var')
     showPlot=0;
@@ -34,9 +38,9 @@ end
 
 % REMEMBER TO REMOVE ------------------------------------------------------
 if regexp(path,'/home/luca/','once')   % for Luca
-    imFolder = '/home/luca/Matlab/iaasfog/Images03';
-    imName = 'frame0000.png';
-    imNum = 50;
+    imFolder = '/home/luca/Matlab/iaasfog/Images';
+    imName = 'frame0000.jpg';
+    imNum = 40;
     exec_path = ['c++/Debug/', bin_name];
     
 elseif regexp(path,'/Users/stefanocadario','once')  % for Stefano
@@ -64,11 +68,10 @@ end
 % checks the image list
 imPaths = getPaths(imFolder, imName, imNum);
 
-alg = 4;
-% alg = selectAlg({'inspect features'; ...
-%     'plot contrasts'; ...
-%     'estimate lambda by fitting'; ...
-%     'normalize by fitted k and then ransac'});
+alg = selectAlg({'inspect features'; ...
+    'plot contrasts'; ...
+    'estimate lambda by fitting'; ...
+    'normalize by fitted k and then ransac'});
 
 if GUI || exist(outFile,'file')~=2
     disp('Computing image features. Could take some time and open funny windows...');
